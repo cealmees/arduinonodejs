fdd7a3c15c31bec86867442d171756910bdb8eb7
Cesar Alejandro Meza <cealmees@outlook.com>
2016-10-20 03:23:03 -0400

    Minor fixes for node server.

--- NodeServer/index.js
+++ NodeServer/index.js
@@ -2,24 +2,24 @@ var express = require('express');
 var app = express();
 var url = require('url');
 var http = require('http').Server(app);
-var io = require('socket.io')(http);
-var TEMP_LIMIT = 28;
-
+var io = require('socket.io') (http);
 var port = 8080;
 
-var temp;
-
-io.on('connection', function(socket) {
+io.on('connection', function (socket){
     
+    var socketID = socket.id;
+    var client = socket.request.connection.remoteAddress;
+    console.log('a user connected. Socket ID: ' + socketID + "|| Cliente: " + client );
+
     app.get('/temp', function(req, res) {
         var getValues = req.url;
         if(getValues.indexOf('/temp') !== -1)
         {
+            res.write('|normal|'); 
             var parsedURL = url.parse(getValues, true);
-            var params = parsedURL.query || {};
+            var params = parsedURL.query;
+
             var d = new Date();
-            console.log(params.value[2]);
-            temp = params.value || null;
             
             var arduinoValues = {
                 ServerTime : d.toLocaleString(),  
@@ -27,37 +27,24 @@ io.on('connection', function(socket) {
                 Humidity : params.value[1], 
                 Light : params.value[2] 
             };
+
             console.log(arduinoValues);
-            var message = '['+d.toLocaleString()+'] Temperatura: '+ arduinoValues.ServerTime +'';
             io.emit('data sender', arduinoValues);
-            if (temp) {
-                if (temp > TEMP_LIMIT) {
-                    console.log('|warning| '+ message);
-                    res.write('|warning|');
-                } else {
-                    console.log('|normal|'+ message);
-                    res.write('|normal|');
-                }
-            } 
         }
         res.end();  
-    });
-});
+    }
+    );
 
-
-io.on('connection', function(socket){
-    var socketID = socket.id;
-var client = socket.request.connection.remoteAddress;
-  console.log('a user connected. Socket ID: ' + socketID + "|| Cliente: " + client );
-  socket.on('disconnect', function(){
+    socket.on('disconnect', function() {
     console.log('user disconnected');
-  });
-});
 
+  });
+
+});
 
 
 app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/public/index.html');
+    res.sendFile(__dirname + '/index.html');
 });
 
 http.listen(port, function() {
